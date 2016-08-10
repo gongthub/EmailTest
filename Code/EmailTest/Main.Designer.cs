567ab821b138f932d93e5f0d8b1d1cb4f3fe626f
gongthub <1056685839@qq.com>
2016-08-10 09:45:50 +0800

    Add

---
+++ Code/EmailTest/Main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace EmailTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbInfo = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFromUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLogs = new System.Windows.Forms.TextBox();
+            this.gbInfo.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbInfo
+            // 
+            this.gbInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbInfo.Controls.Add(this.btnSend);
+            this.gbInfo.Controls.Add(this.label3);
+            this.gbInfo.Controls.Add(this.txtToUser);
+            this.gbInfo.Controls.Add(this.label2);
+            this.gbInfo.Controls.Add(this.txtFromUser);
+            this.gbInfo.Controls.Add(this.label1);
+            this.gbInfo.Controls.Add(this.txtServer);
+            this.gbInfo.Location = new System.Drawing.Point(12, 1);
+            this.gbInfo.Name = "gbInfo";
+            this.gbInfo.Size = new System.Drawing.Size(660, 78);
+            this.gbInfo.TabIndex = 0;
+            this.gbInfo.TabStop = false;
+            this.gbInfo.Text = "信息";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(551, 15);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 51);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "收件人：";
+            // 
+            // txtToUser
+            // 
+            this.txtToUser.Location = new System.Drawing.Point(62, 45);
+            this.txtToUser.Name = "txtToUser";
+            this.txtToUser.Size = new System.Drawing.Size(440, 21);
+            this.txtToUser.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "发件人：";
+            // 
+            // txtFromUser
+            // 
+            this.txtFromUser.Location = new System.Drawing.Point(352, 18);
+            this.txtFromUser.Name = "txtFromUser";
+            this.txtFromUser.Size = new System.Drawing.Size(150, 21);
+            this.txtFromUser.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器：";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(62, 18);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(150, 21);
+            this.txtServer.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLogs);
+            this.groupBox1.Location = new System.Drawing.Point(13, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(659, 314);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日志";
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLogs.Location = new System.Drawing.Point(3, 17);
+            this.txtLogs.Multiline = true;
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.Size = new System.Drawing.Size(653, 294);
+            this.txtLogs.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbInfo);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "邮件发送测试";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.gbInfo.ResumeLayout(false);
+            this.gbInfo.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFromUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLogs;
+    }
+}
+
---
+++ Code/EmailTest/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailTest
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtServer.Text = "smtp.exmail.qq.com";
+            txtFromUser.Text = "testadmin@vlinker.com.cn";
+            txtToUser.Text = "gongtao@vlinker.com.cn";
+            txtFromUser.Enabled = false;
+        }
+
+        /// <summary>
+        /// 发送按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string server = txtServer.Text;
+            string fromuser = txtFromUser.Text;
+            string tousers = txtToUser.Text.ToString();
+            if (tousers == null || tousers.Trim() == "")
+            {
+                MessageBox.Show("收件人不能为空!");
+            }
+            else
+            {
+                List<string> users = new List<string>();
+                users = tousers.Split(';').ToList();
+                string logs = SendEmail(server, fromuser, users, "测试邮件");
+                txtLogs.Text += logs + "\r\n";
+            }
+        }
+
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="sFrom"></param>
+        /// <param name="sTo"></param>
+        /// <param name="sSubject"></param>
+        /// <param name="sMessageBody"></param>
+        public static string SendEmail(string server, string sFrom, List<string> sTo, string sSubject)
+        {
+            try
+            {
+                if (sTo != null && sTo.Count > 0)
+                {
+                    string pwds = "Vlinker123";//密码
+                    MailAddress from = new MailAddress(sFrom);
+                    using (MailMessage message = new MailMessage())
+                    {
+                        message.From = from;
+                        foreach (string s in sTo)
+                        {
+                            MailAddress to = new MailAddress(s);
+                            message.To.Add(to);
+                        }
+
+                        message.Body = "<p>发送测试邮件 （操作者： " + sFrom + "）</p>";
+                        message.Subject = sSubject;
+                        message.IsBodyHtml = true;
+
+                        SmtpClient client = new SmtpClient(server);
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.Credentials = new System.Net.NetworkCredential(sFrom, pwds);
+                        client.Send(message);
+                    }
+                }
+
+                return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送成功";
+            }
+            catch (Exception e)
+            {
+                return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "：邮件发送失败:" + e.InnerException.Message;
+            }
+        }
+    }
+}
